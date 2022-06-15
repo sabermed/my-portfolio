4367fd0ffed5e6f02817a77d48a034289ed577eb
Saber Mohamed <sabermed66@gmail.com>
2022-06-15 22:19:25 +0100

    getting nav(not responsive yet) and section ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="nav-section">
+          <span className="logo">Saber Mohamed</span>
+          <ul className="nav-bar">
+            <i className="nav-link">HOME</i>
+            <i className="nav-link">WORK</i>
+            <i className="nav-link">ABOUT</i>
+            <i className="nav-link">CONTACT</i>
+            <i className="nav-link">
+              <button className="get-in-touch-button">GET IN TOUCH</button>
+            </i>
+          </ul>
+        </div>
+        <div id="fp-nav" className="left">
+          <ul>
+            <li>
+              <a href="#top" className="active">
+                <span></span>
+              </a>
+            </li>
+            <li>
+              <a href="#reile" className="">
+                <span></span>
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="">
+                <span></span>
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="">
+                <span></span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="contact" className="contact"></div>
+    </>
   );
 }
 
